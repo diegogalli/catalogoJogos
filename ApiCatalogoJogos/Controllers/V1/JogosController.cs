3e969354cd7972bfea204daa02a17ebf273244b4
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-07-28 14:37:42 -0300

    Atualizando Controller e acrescentando Models

--- ApiCatalogoJogos/Controllers/V1/JogosController.cs
+++ ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,25 +14,25 @@ namespace ApiCatalogoJogos.Controllers.V1
     public class JogosController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<object>>> Obter()
+        public async Task<ActionResult<List<JogoViewModel>>> Obter()
         {
             return Ok();
         }
 
         [HttpGet("{idJogo:guid}")]
-        public async Task<ActionResult<List<object>>> Obter(Guid idJogo)
+        public async Task<ActionResult<List<JogoViewModel>>> Obter(Guid idJogo)
         {
             return Ok();
         }
 
         [HttpPost]
-        public async Task<ActionResult<object>> InserirJogo( object jogo)
+        public async Task<ActionResult<JogoViewModel>> InserirJogo( JogoInputModel jogo)
         {
             return Ok();
         }
 
         [HttpPut("{idJogo:guid}")]
-        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, JogoInputModel jogo)
         {
             return Ok();
         }
